d8f54a5b0432137533268e584ba7d052392b7f04
magicGAT <magicgat@msn.com>
2020-01-21 20:27:55 -0800

    small update

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Job model import statement to pull data from the project database
+from .models import Job
+
+def home(request):
+	jobs = Job.objects
+	return render(request, 'jobs/home.html', {'jobs':jobs})
